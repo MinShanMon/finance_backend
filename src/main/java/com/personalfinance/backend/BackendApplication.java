5b9b8cab615923a1687ab6025eceae53e92ddaf3
Your Name <shanmon2017@gmail.com>
2023-02-09 12:12:56 +0800

    last update

--- src/main/java/com/personalfinance/backend/BackendApplication.java
+++ src/main/java/com/personalfinance/backend/BackendApplication.java
@@ -45,34 +45,34 @@ public class BackendApplication {
 		return args -> {
 
 
-			Bank dbs = bankRepository.saveAndFlush(new Bank("dbs", "https://dbs.com"));
-			Bank uob = bankRepository.saveAndFlush(new Bank("uob", "https://uob.com"));
-			Bank ocbc = bankRepository.saveAndFlush(new Bank("ocbc", "https://ocbc.com"));
-			Bank posb = bankRepository.saveAndFlush(new Bank("posb", "https://posb.com"));
+			// Bank dbs = bankRepository.saveAndFlush(new Bank("dbs", "https://dbs.com"));
+			// Bank uob = bankRepository.saveAndFlush(new Bank("uob", "https://uob.com"));
+			// Bank ocbc = bankRepository.saveAndFlush(new Bank("ocbc", "https://ocbc.com"));
+			// Bank posb = bankRepository.saveAndFlush(new Bank("posb", "https://posb.com"));
 
 
-			LocalDateTime now = LocalDateTime.now(); 
-			FixedDeposits fd1 = fixedDepositsRepository.saveAndFlush(new FixedDeposits(6,10,999,0.02,now,dbs));
-			FixedDeposits fd2 = fixedDepositsRepository.saveAndFlush(new FixedDeposits(12,10,969,0.02,now,dbs));
-			FixedDeposits fd3 = fixedDepositsRepository.saveAndFlush(new FixedDeposits(6,10,95599,0.02,now,uob));	
-			FixedDeposits fd4 = fixedDepositsRepository.saveAndFlush(new FixedDeposits(8,10,239,0.02,now,ocbc));	
-			FixedDeposits fd5 = fixedDepositsRepository.saveAndFlush(new FixedDeposits(12,10,2399,0.02,now,posb));	
-			FixedDeposits fd6 = fixedDepositsRepository.saveAndFlush(new FixedDeposits(6,10,9199,0.02,now,dbs));
-			FixedDeposits fd7 = fixedDepositsRepository.saveAndFlush(new FixedDeposits(12,10,299,0.02,now,dbs));
-			FixedDeposits fd8 = fixedDepositsRepository.saveAndFlush(new FixedDeposits(6,10,909,0.02,now,uob));	
-			FixedDeposits fd9 = fixedDepositsRepository.saveAndFlush(new FixedDeposits(8,10,239,0.02,now,ocbc));	
-			FixedDeposits fd0 = fixedDepositsRepository.saveAndFlush(new FixedDeposits(12,10,499,0.02,now,posb));	
-			fixedDepositsRepository.saveAndFlush(fd1);
-			fixedDepositsRepository.saveAndFlush(fd2);
-			fixedDepositsRepository.saveAndFlush(fd3);
-			fixedDepositsRepository.saveAndFlush(fd4);
-			fixedDepositsRepository.saveAndFlush(fd5);
+			// LocalDateTime now = LocalDateTime.now(); 
+			// FixedDeposits fd1 = fixedDepositsRepository.saveAndFlush(new FixedDeposits(6,10,999,0.02,now,dbs));
+			// FixedDeposits fd2 = fixedDepositsRepository.saveAndFlush(new FixedDeposits(12,10,969,0.02,now,dbs));
+			// FixedDeposits fd3 = fixedDepositsRepository.saveAndFlush(new FixedDeposits(6,10,95599,0.02,now,uob));	
+			// FixedDeposits fd4 = fixedDepositsRepository.saveAndFlush(new FixedDeposits(8,10,239,0.02,now,ocbc));	
+			// FixedDeposits fd5 = fixedDepositsRepository.saveAndFlush(new FixedDeposits(12,10,2399,0.02,now,posb));	
+			// FixedDeposits fd6 = fixedDepositsRepository.saveAndFlush(new FixedDeposits(6,10,9199,0.02,now,dbs));
+			// FixedDeposits fd7 = fixedDepositsRepository.saveAndFlush(new FixedDeposits(12,10,299,0.02,now,dbs));
+			// FixedDeposits fd8 = fixedDepositsRepository.saveAndFlush(new FixedDeposits(6,10,909,0.02,now,uob));	
+			// FixedDeposits fd9 = fixedDepositsRepository.saveAndFlush(new FixedDeposits(8,10,239,0.02,now,ocbc));	
+			// FixedDeposits fd0 = fixedDepositsRepository.saveAndFlush(new FixedDeposits(12,10,499,0.02,now,posb));	
+			// fixedDepositsRepository.saveAndFlush(fd1);
+			// fixedDepositsRepository.saveAndFlush(fd2);
+			// fixedDepositsRepository.saveAndFlush(fd3);
+			// fixedDepositsRepository.saveAndFlush(fd4);
+			// fixedDepositsRepository.saveAndFlush(fd5);
 
-			fixedDepositsRepository.saveAndFlush(fd6);
-			fixedDepositsRepository.saveAndFlush(fd7);
-			fixedDepositsRepository.saveAndFlush(fd8);
-			fixedDepositsRepository.saveAndFlush(fd9);
-			fixedDepositsRepository.saveAndFlush(fd0);
+			// fixedDepositsRepository.saveAndFlush(fd6);
+			// fixedDepositsRepository.saveAndFlush(fd7);
+			// fixedDepositsRepository.saveAndFlush(fd8);
+			// fixedDepositsRepository.saveAndFlush(fd9);
+			// fixedDepositsRepository.saveAndFlush(fd0);
 
 	   };
 
--- src/test/java/com/personalfinance/backend/dbs.java
+++ src/test/java/com/personalfinance/backend/dbs.java
@@ -6,13 +6,10 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 
-
 public class dbs {
     public static void main(String[] args) throws IOException{
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.dbs.com.sg/personal/rates-online/singapore-dollar-fixed-deposits.page");
         System.out.println(driver.getPageSource());
 }
-
-
 }
